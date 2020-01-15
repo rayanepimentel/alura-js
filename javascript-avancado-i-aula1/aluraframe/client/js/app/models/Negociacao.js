29103d5339674e4610e248a50a2a2cb7b53a7faa
radharay <radharayg@gmail.com>
2020-01-15 10:43:44 -0300

    Aula 02.03 passando os parâmetros para o contructor()

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -4,10 +4,27 @@
 //a data  por padrão terá a data atual do sistema new Date()
 //valor quantidade será 01 e valo 0.0
 
-class Negociacao {
+
+/*class Negociacao {
     constructor() {
         this.date = new Date();
         this.quantidade = 1;
         this.valor = 0.0;
     }
+
+}*/
+
+// existe outra forma de passar os dados para uma instância de negociação
+//queremos que seja definida a quantidade, valor e data
+//que sejam passados imediatamente p constructor()
+// passar os parâmetros para constructor
+// e colocar(passar os param.) nas propriedades do meu objeto
+
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        this.date = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
 }
