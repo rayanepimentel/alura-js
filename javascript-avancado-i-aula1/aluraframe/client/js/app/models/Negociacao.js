7de68fcd82792c7b4db6fced23770521fbe89aa7
radharay <radharayg@gmail.com>
2020-01-17 10:28:44 -0300

    Aula 02.05 - A sintaxe get

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,34 +1,36 @@
-//as propriedades que contenham _ só poderão ser acessadas pelos 
-//próprios métodos da classe. 
-//Isto significa, que mesmo podendo imprimir a propriedade _quantidade 
-//com outro valor, não deveríamos mais poder acessá-la. 
-//O _ funciona como um aviso dizendo: "programador, você não pode alterar esta propriedade!".
+
 class Negociacao {
+
     constructor(data, quantidade, valor) {
-        this._date = data;
+
+        this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
 
-    //métodos acessadores, utilizando o prefixo get
-    //os métodos da classe poderão acessar os atributos que levam _. 
-    //no entanto, de acordo com a nossa convenção, alguém fora da classe não poderá fazer o mesmo.
-    // atributo privados;
+    //Nos criamos metodos de leitura para acessar as variáveis, que são privadas. 
+    //Ou seja só a própria class pode acessa-la. 
+    //Mas podemos criar um atalho para acessar a propriedade somente leitura, criar o get
+    //ex: get volume(){}
 
-    getVolume() {
+    // colocamos um get seguido de um método
+    //este método tem o nome original da nossa propriedade, antes de usarmos o _ 
+    //Quando eu uso este get antes do método, eu acesso esse método como se fosse uma propriedade.
+
+     get volume() {
         return this._quantidade * this._valor; 
     }
     
-    getData() {
-        return this._data
+    get data() {
+        return this._data;
     }
 
-    getQuantidade() {
-        return this._quantidade
+    get quantidade() {
+        return this._quantidade;
     }
 
-    getValor() {
-        return this.valor
+    get valor() {
+        return this._valor;
     }
 
 }
