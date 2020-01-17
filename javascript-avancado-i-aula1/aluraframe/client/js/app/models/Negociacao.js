0e24ea5e5f9336955bb8a7e52cf2c3cddc6c4b40
radharay <radharayg@gmail.com>
2020-01-17 09:20:37 -0300

    Aula 02.06 Atributos privado.

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,30 +1,34 @@
-//cria a class e defino os atributos da minha negociação
-//defino pela função constructor
-//que ela vai ter uma data, quantiddade e valor this.
-//a data  por padrão terá a data atual do sistema new Date()
-//valor quantidade será 01 e valo 0.0
-
-
-/*class Negociacao {
-    constructor() {
-        this.date = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
-    }
-
-}*/
-
-// existe outra forma de passar os dados para uma instância de negociação
-//queremos que seja definida a quantidade, valor e data
-//que sejam passados imediatamente p constructor()
-// passar os parâmetros para constructor
-// e colocar(passar os param.) nas propriedades do meu objeto
-
+//as propriedades que contenham _ só poderão ser acessadas pelos 
+//próprios métodos da classe. 
+//Isto significa, que mesmo podendo imprimir a propriedade _quantidade 
+//com outro valor, não deveríamos mais poder acessá-la. 
+//O _ funciona como um aviso dizendo: "programador, você não pode alterar esta propriedade!".
 class Negociacao {
     constructor(data, quantidade, valor) {
-        this.date = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._date = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    //métodos acessadores, utilizando o prefixo get
+    //os métodos da classe poderão acessar os atributos que levam _. 
+    //no entanto, de acordo com a nossa convenção, alguém fora da classe não poderá fazer o mesmo.
+    // atributo privados;
+
+    getVolume() {
+        return this._quantidade * this._valor; 
+    }
+    
+    getData() {
+        return this._data
+    }
+
+    getQuantidade() {
+        return this._quantidade
+    }
+
+    getValor() {
+        return this.valor
     }
 
 }
